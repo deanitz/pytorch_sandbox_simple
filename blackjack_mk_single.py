510715cc463a490c2435a3d70903f0118a63435d
Denis Mikhaylichenko <denis.mikhaylichenko@finastra.com>
2020-11-18 15:01:38 +0300

    (blackjack): added MC single-Q solving - estimates

--- blackjack_mk_single.py
+++ blackjack_mk_single.py
@@ -64,8 +64,55 @@ def mc_control_on_polcy(env, gamma, n_episode):
 
     return Q, policy
 
+def simulate_episode(env, policy):
+    state = env.reset()
+    is_done = False
+    while not is_done:
+        action = policy[state]
+        state, reward, is_done, info = env.step(action)
+        if is_done:
+            return reward
+
 gamma = 1
-n_episodes = 500_000
+n_episodes = 1_000_000
 
 optimal_Q, optimal_policy = mc_control_on_polcy(env, gamma, n_episodes)
-print(optimal_policy)
+for s in sorted(optimal_policy):
+    print(s, optimal_policy[s])
+
+print(len(optimal_policy))
+
+hold_score = 18
+hold_policy = {}
+for player in range(2, 22):
+    for dealer in range(1, 11):
+        action = 1 if player <= hold_score else 0
+        hold_policy[(player, dealer, False)] = action
+        hold_policy[(player, dealer, True)] = action
+
+n_episodes = 100_000
+n_win_optimal = 0
+n_win_simple = 0
+n_lose_optimal = 0
+n_lose_simple = 0
+
+for _ in range(n_episodes):
+    reward = simulate_episode(env, optimal_policy)
+    if reward == 1:
+        n_win_optimal += 1
+    elif reward == -1:
+        n_lose_optimal += 1
+    
+    reward = simulate_episode(env, hold_policy)
+    if reward == 1:
+        n_win_simple += 1
+    elif reward == -1:
+        n_lose_simple += 1
+
+print('Вероятность выигрыша при простой стратегии: {}'.format(n_win_simple / n_episodes))
+print('Вероятность выигрыша при оптимальной стратегии: {}'.format(n_win_optimal / n_episodes))
+print('Вероятность проигрыша при простой стратегии: {}'.format(n_lose_simple / n_episodes))
+print('Вероятность проигрыша при оптимальной стратегии: {}'.format(n_lose_optimal / n_episodes))
+
+
+
